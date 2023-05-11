d4f73555fa920492102be5b5204845bd48432cf7
熊林勇 <2690363124@qq.com>
2023-05-11 22:14:00 +0800

    修改配置增加文档

--- doc/.vitepress/config/sidebar.ts
+++ doc/.vitepress/config/sidebar.ts
@@ -8,7 +8,6 @@ function getSidebarDeep (pathStr:string) {
   fs.readdirSync(getAbsolutePath(pathStr)).forEach((value,index)=>{
     const relativePath=`${pathStr}/${value}`
 
-    if (value==='index.md') return
     const absolutePath=getAbsolutePath(relativePath)
     const stat=fs.statSync(absolutePath)
     if (stat.isFile()) {
