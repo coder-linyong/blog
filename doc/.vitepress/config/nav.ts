8e86f0f41f6f57b378edac179e77edc0a2cccd44
xionglinyong <xionglinyong@skieer.com>
2023-05-11 17:49:18 +0800

    添加注释

--- doc/.vitepress/config/nav.ts
+++ doc/.vitepress/config/nav.ts
@@ -2,7 +2,7 @@ import { DefaultTheme } from 'vitepress/types/default-theme'
 import * as fs from 'fs'
 import { getAbsolutePath } from './util'
 
-
+// 获取头部nav，取doc目录下一级目录作为nav配置，如/js使用的是doc/js/index.md文件，
 export default function getNav(pathStr='/'):DefaultTheme.NavItem[] {
   const excludePaths=[
     '/.vitepress',
--- doc/.vitepress/config/sidebar.ts
+++ doc/.vitepress/config/sidebar.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs'
 import { DefaultTheme } from 'vitepress/types/default-theme'
 import { getAbsolutePath, getFileName } from './util'
 
+// 根据doc目录下的文件夹目录深度获取侧边栏配置
 function getSidebarDeep (pathStr:string) {
   const config=[]
   fs.readdirSync(getAbsolutePath(pathStr)).forEach((value,index)=>{
