698ca06c597456a3281130e79b478ee900bd434c
xionglinyong <xionglinyong@skieer.com>
2023-05-15 18:07:59 +0800

    取消search功能

--- doc/.vitepress/config.ts
+++ doc/.vitepress/config.ts
@@ -13,9 +13,9 @@ export default defineConfig({
   base: '/blog/',
   lastUpdated: true,
   themeConfig: {
-    search: {
-      provider: 'local'
-    },
+    // search: {
+    //   provider: 'local'
+    // },
     // https://vitepress.dev/reference/default-theme-config
     nav: getNav(),
 
